e1d96d071bf3be12eb42cae40858237cd0013a1d
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-01-28 16:28:27 +0300

    Третья задача к семинару 1, исправление

--- HomeWork/Task3HW.py
+++ HomeWork/Task3HW.py
@@ -5,12 +5,12 @@ quarter = int(input('Введите номер четверти: '))
 
 if 1 <= quarter <=4 :
     if quarter == 1:
-        print(f'Диапозон по оси X от {1} до {float("inf")} \nДиапозон по оси Y > от {1} до {float("inf")}')
+        print(f'Диапозон по оси X от {1} до {float("inf")} \nДиапозон по оси Y от {1} до {float("inf")}')
     if quarter == 2:
-        print(f'Диапозон по оси X > от {-1} до {float("-inf")} \nДиапозон по оси Y > от {1} до {float("inf")}')
+        print(f'Диапозон по оси X от {-1} до {float("-inf")} \nДиапозон по оси Y от {1} до {float("inf")}')
     if quarter == 3:
-        print(f'Диапозон по оси X > от {-1} до {float("-inf")} \nДиапозон по оси Y > от {-1} до {float("-inf")}')
+        print(f'Диапозон по оси X от {-1} до {float("-inf")} \nДиапозон по оси Y от {-1} до {float("-inf")}')
     if quarter == 4:
-        print(f'Диапозон по оси X > от {1} до {float("inf")} \nДиапозон по оси Y > от {-1} до {float("-inf")}')
+        print(f'Диапозон по оси X от {1} до {float("inf")} \nДиапозон по оси Y от {-1} до {float("-inf")}')
 else:
     print('Координаты не подходят!')
