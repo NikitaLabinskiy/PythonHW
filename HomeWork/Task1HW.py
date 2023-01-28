78646d657b66dedf06828df0499e5b7df2b7dab8
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-01-28 16:11:21 +0300

    Первая задача к семинару 1

---
+++ HomeWork/Task1HW.py
@@ -0,0 +1,25 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+# и проверяет, является ли этот день выходным.
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+number = int(input())
+
+if number == 1:
+    print(f'Да - {number}')
+elif number == 2:
+    print(f'Да - {number}')
+elif number == 3:
+    print(f'Да - {number}')
+elif number == 4:
+    print(f'Да - {number}')
+elif number == 5:
+    print(f'Да - {number}')
+elif number == 6:
+    print(f'Нет - {number}')
+elif number == 7:
+    print(f'Нет - {number}')
+else:
+    print('Число не подходит')
