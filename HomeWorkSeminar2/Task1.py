ea30f53a7ae7bbafa265baa903084e75a07e678f
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-01-30 17:26:26 +0300

    Первая задача

---
+++ HomeWorkSeminar2/Task1.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# - 6782 -> 23
+# - 0,56 -> 11
+
+number = input()
+count = 0
+number = float(number[::-1])
+print((number))
+
+while number != 0:
+    count += number % 10
+    number = number // 10
+
+print(count)
