52350637c13bc3f68d8d692b2664ad109e5398e0
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-02-05 15:21:15 +0300

    Добавил 4 задачу из ДЗ к 3 семинару

--- HomeWorkSeminar3/Task4.py
+++ HomeWorkSeminar3/Task4.py
@@ -7,10 +7,16 @@
 number = int(input())
 number1 = number
 result = ''
-while number >= 1:
-    if number % 2 == 0:
-        result += '0'
-    else:
-        result += '1'
+# while number >= 1:
+#     if number % 2 == 0:
+#         result += '0'
+#     else:
+#         result += '1'
+#     number //= 2
+# print(f'Число: {number1} в двоичной системе = {result[::-1]}')
+
+while number > 0:
+    result = str(number % 2) + result
     number //= 2
-print(f'Число: {number1} в двоичной системе = {result[::-1]}')
+
+print(f'Число: {number1} в двоичной системе = {result}')
